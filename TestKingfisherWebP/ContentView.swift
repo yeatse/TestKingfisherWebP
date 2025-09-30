31b442a6d3b981fb736cda29e884dca42bb14f48
Yang Chao <iyeatse@gmail.com>
2025-09-30 23:59:01 +0800

    add files

--- TestKingfisherWebP/ContentView.swift
+++ TestKingfisherWebP/ContentView.swift
@@ -6,17 +6,36 @@
 //
 
 import SwiftUI
+import Kingfisher
+import KingfisherWebP
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            WebPImageView()
+                .frame(width: 200, height: 200)
         }
-        .padding()
     }
+}
+
+struct WebPImageView: UIViewRepresentable {
+    func makeUIView(context: Context) -> UIImageView {
+        let animatedView = AnimatedImageView()
+        animatedView.contentMode = .scaleAspectFit
+        animatedView.backgroundColor = .lightGray
+
+        // 本地 bundle 文件 URL
+        if let url = Bundle.main.url(forResource: "heart", withExtension: "webp") {
+            animatedView.kf.setImage(with: url, options: [
+                .processor(WebPProcessor.default),
+                .cacheSerializer(WebPSerializer.default)
+            ])
+        }
+
+        return animatedView
+    }
+
+    func updateUIView(_ uiView: UIImageView, context: Context) {}
 }
 
 #Preview {
