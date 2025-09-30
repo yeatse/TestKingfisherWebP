a684084df2f5a5fcc7277789ab54f4e63a9e314b
Yang Chao <iyeatse@gmail.com>
2025-09-30 23:31:40 +0800

    Initial Commit

---
+++ TestKingfisherWebP/TestKingfisherWebPApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestKingfisherWebPApp.swift
+//  TestKingfisherWebP
+//
+//  Created by yeatse on 2025/9/30.
+//
+
+import SwiftUI
+
+@main
+struct TestKingfisherWebPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
